d13f44b138f8148be3380df478214f5eeaae21a1
Gian Marco Mari <gianmarcomari@gmail.com>
2015-11-04 12:09:37 +0100

    Batch Command for Execut Tool Backup on Server without CMD line
    Batch Command for Execut Tool Backup on Server without CMD line

--- Executable Backup on Server/Backup.bat
+++ Executable Backup on Server/Backup.bat
@@ -1,3 +1,3 @@
 :: Executable.cmd
 :: Put here your program directory path                                   Put here your comand
-start /D "C:\Users\Electric80\Documents\ToolBackup\" Backup_on_Server.exe VS2013
+start /D "C:\Users\GianMarco\Documents\ToolBackup\" Backup_on_Server.exe VS2013
